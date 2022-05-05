9fc707856717f9bd34c89fbf8233fca21513bb19
angel <angelxd0714@gmail.com>
2022-05-05 17:20:16 -0500

    se sube cambios

--- controllers/actualizar.productos.js
+++ controllers/actualizar.productos.js
@@ -1,20 +1,18 @@
 import { functPro } from '../services/cliente.servidor.js';
 const formulario = document.querySelector('[ data-form]');
-let retorno = '';
+const urlact = document.querySelector('[data-imagen]');
+const nombre = document.querySelector('[data-nombre]');
+const precio = document.querySelector('[data-precio]');
+const categoria = document.querySelector('.lista_despegable');
+const desc = document.querySelector('[data-desc]');
 export const verDetalleproducto = () => {
     const urls = new URL(window.location);
     const id = urls.searchParams.get("id");
     if (id === null) {
         console.log('no hay nada');
     }
-    const urlact = document.querySelector('[data-imagen]');
-    const nombre = document.querySelector('[data-nombre]');
-    const precio = document.querySelector('[data-precio]');
-    const categoria = document.querySelector('.lista_despegable');
-    const desc = document.querySelector('[data-desc]');
-
     functPro.detalleProducto(id).then((producto) => {
-        urlact.value = producto.url;
+
         nombre.value = producto.nombre;
         precio.value = producto.precio;
         categoria.value = producto.categoria;
@@ -29,10 +27,7 @@ verDetalleproducto();
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault();
     console.log(modificarProducto());
-    try {
-        functPro.actualizarProducto(urlact.value, nombre.value, precio.value, categoria.value, desc.value, id)
-        window.location.href = ("producto.html");
-    } catch (error) {
-        console.log(error)
-    }
+    functPro.actualizarProducto(nombre.value, precio.value, categoria.value, desc.value, id)
+    window.location.href = ("producto.html");
+
 });
